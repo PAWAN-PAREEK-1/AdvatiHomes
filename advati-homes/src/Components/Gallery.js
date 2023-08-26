825cd10976be0e49d62fab3c1a209a0a1e40d344
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-08-26 22:12:46 +0530

    gallery section update

--- advati-homes/src/Components/Gallery.js
+++ advati-homes/src/Components/Gallery.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useRef } from 'react';
 import "../Style/Gallery.css";
 import right from "../Assets/img/rightarrow.svg";
 import left from "../Assets/img/leftarrow.svg";
@@ -6,38 +6,57 @@ import a1 from "../Assets/img/a1.svg";
 import a2 from "../Assets/img/a2.svg";
 import a3 from "../Assets/img/a3.svg";
 import a4 from "../Assets/img/a4.svg";
+import rightarrow from "../Assets/img/grightsvg.svg";
 
 const Gallery = () => {
+  const images = [a1, a2, a3, a4, a2, a3, a4];
+  const imageRef = useRef(null);
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleNextClick = () => {
+    if (imageRef.current) {
+      imageRef.current.scrollBy({ left: 200, behavior: 'smooth' });
+      setScrollPosition(scrollPosition + 200);
+    }
+  };
+
+  const handlePrevClick = () => {
+    if (imageRef.current) {
+      imageRef.current.scrollBy({ left: -200, behavior: 'smooth' });
+      setScrollPosition(scrollPosition - 200);
+    }
+  };
+
   return (
     <div>
-        <div className="gallery-index">
-                <div className="gallery-up">
-                    <div className="gallery-head">
-                            <h3>Gallary <hr /></h3>
-                            <h1>Our Recent Projects</h1>
-                    </div>
-                    <div className="navigation">
-                        <a href=""><img src={left} alt="" /></a>
-                       <a href=""> <img src={right} alt="" /></a>
-                    </div>
-
-                </div>
-                <div className="gallery-down">
-                <img src={a1} alt="" />
-                <img src={a2} alt="" />
-                <img src={a3} alt="" />
-                <img src={a4} alt="" />
-                <img src={a2} alt="" />
-                <img src={a2} alt="" />
-                <img src={a3} alt="" />
-                <img src={a4} alt="" />
-                <img src={a2} alt="" />
-
-
-                </div>
+      <div className="gallery-index">
+        <div className="gallery-up">
+          <div className="gallery-head">
+            <h3>Gallery <hr /></h3>
+            <h1>Our Recent Projects</h1>
+          </div>
+          <div className="navigation">
+            <img onClick={handlePrevClick} src={left} alt="Left Arrow" />
+            <img onClick={handleNextClick} src={right} alt="Right Arrow" />
+          </div>
         </div>
+        <div className="gallery-down" ref={imageRef}>
+          <div className="image-container" style={{ transform: `translateX(-${scrollPosition}px)` }}>
+            {images.map((image, index) => (
+              <div className="image-wrapper" key={index}>
+                <img
+                  src={image}
+                  alt={`Image ${index + 1}`}
+                  className="gallery-image"
+                />
+                <div className="image-tooltip"><h2>Learn more <img src={rightarrow} alt="" /> </h2></div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Gallery
+export default Gallery;
