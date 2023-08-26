f3fc559a4627e3119e82794b27e3ec0ea694ef3b
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-08-26 12:54:39 +0530

    index section

--- advati-homes/src/Pages/Home.js
+++ advati-homes/src/Pages/Home.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import { useRef, useState ,lazy, Suspense } from "react";
+import React, { useRef, useState, useEffect, lazy, Suspense } from "react";
 import "../Style/Home.css";
 import shadebg from "../Assets/img/smallherobg.svg";
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -8,47 +7,75 @@ import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
 import bg from "../Assets/img/herobg.svg";
 import bg2 from "../Assets/img/herobg2.svg";
+import arrow from "../Assets/img/twoarrow.svg";
+import mouse from "../Assets/img/mouse.svg";
 const Navbar = lazy(() => import("../Components/Navbar"));
 const Loading = lazy(() => import("../Components/Loading"));
 
 const Home = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 0) {
+      setScrolled(true);
+      setTimeout(() => {
+        setScrolled(false);
+      }, 50000);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleReturnToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    setScrolled(false);
+  };
+
   return (
     <Suspense
-    fallback={
+      fallback={
+        <div>
+          <Loading />
+        </div>
+      }
+    >
       <div>
-        <Loading />
-      </div>
-    }>
+        <div className="index-hero">
+          <Navbar activeUrl={"/"} />
+          <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+            <SwiperSlide><img src={bg} alt="" /></SwiperSlide>
+            <SwiperSlide><img src={bg2} alt="" /></SwiperSlide>
+            <SwiperSlide><img src={bg} alt="" /></SwiperSlide>
+          </Swiper>
 
-
-
-
-    <div>
-            <div className="index-hero">
-            <Navbar activeUrl={"/"}/>
-            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-
-
-        <SwiperSlide><img src={bg} alt="" /></SwiperSlide>
-        <SwiperSlide><img src={bg2} alt="" /></SwiperSlide>
-        <SwiperSlide><img src={bg} alt="" /></SwiperSlide>
-       
-      </Swiper>
-
-
-              <div className="bg-hero">
-                <img src={shadebg} alt="" />
-              </div>
-
+          <div className="swiper-details">
+            <h1>Welcome to Advait Homes</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pretium mollis feugiat. Aenean sed fermentum orci, accumsan feugiat justo. </p>
+            <div className="quote-btn quote-btn2 cursor-pointer">
+              <a href="">Get Quotes <img src={arrow} alt="" /> </a>
             </div>
+          </div>
 
+          {!scrolled && (
+            <div className="mouse">
+              <img src={mouse} alt="" onClick={handleReturnToTop} />
+            </div>
+          )}
 
-
-    </div>
-
-
+          <div className="bg-hero">
+            <img src={shadebg} alt="" />
+          </div>
+        </div>
+      </div>
     </Suspense>
-  )
+  );
 }
 
-export default Home
+export default Home;
