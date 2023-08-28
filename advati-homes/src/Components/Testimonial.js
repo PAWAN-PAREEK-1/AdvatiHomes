ced8243accf5f8a79c75fc6d1369d66cde12f60b
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-08-28 22:52:26 +0530

    footer

--- advati-homes/src/Components/Testimonial.js
+++ advati-homes/src/Components/Testimonial.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../Style/Testimonial.css";
 import t1 from "../Assets/img/t1.svg";
 import t2 from "../Assets/img/t2.svg";
@@ -8,51 +8,71 @@ import testi from "../Assets/img/testi.svg";
 import tright from "../Assets/img/tright.svg";
 import tleft from "../Assets/img/tleft.svg";
 
-const Testimonial = () => {
-  return (
-    <div>
-      <div className="testi-index">
-        <div className="testimonial">
-            <img src={tright} alt="" id="tright"/>
-            <img src={tleft} alt="" id="tleft" />
+const testimonials = [
+    {
+      imgSrc: t1,
+      name: "Hanna Franci",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis nulla interdum orci congue rlibero. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      imgSrc: "../Assets/img/t2.svg",
+      name: "John Doe",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis nulla interdum orci congue rlibero. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      imgSrc: "../Assets/img/t3.svg",
+      name: "Jane Smith",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis nulla interdum orci congue rlibero. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+  ];
 
-          <div className="testi-box">
-            <img src={t1} alt="" />
-            <img src={star} alt="" />
-            <h3>Hanna Franci</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-              quis nulla interdum orci congue rlibero. Lorem ipsum dolor sit
-              amet, consectetur adipiscing elit.
-            </p>
+  const Testimonial = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-          </div>
-          <div className="testi-box testi-center">
-            <img src={t1} alt="" />
-            <img src={star} alt="" />
-            <h3>Hanna Franci</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-              quis nulla interdum orci congue rlibero. Lorem ipsum dolor sit
-              amet, consectetur adipiscing elit.
-            </p>
-            <img src={testi} alt="" id="testi" />
-          </div>
-          <div className="testi-box">
-            <img src={t1} alt="" />
-            <img src={star} alt="" />
-            <h3>Hanna Franci</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-              quis nulla interdum orci congue rlibero. Lorem ipsum dolor sit
-              amet, consectetur adipiscing elit.
-            </p>
+    const handleNext = () => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % testimonials.length);
+    };
 
+    const handlePrev = () => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === 0 ? testimonials.length - 1 : prevIndex - 1
+      );
+    };
+
+    const currentTestimonial = testimonials[currentIndex];
+
+    return (
+      <div>
+        <div className="testi-index">
+          <div className="testimonial">
+            <img src={tright} alt="" id="tright" onClick={handleNext} />
+            <img src={tleft} alt="" id="tleft" onClick={handlePrev} />
+
+            <div className="testi-box">
+              <img src={currentTestimonial.imgSrc} alt="" />
+              <img src={star} alt="" />
+              <h3>{currentTestimonial.name}</h3>
+              <p>{currentTestimonial.content}</p>
+            </div>
+            <div className="testi-box testi-center">
+              <img src={currentTestimonial.imgSrc} alt="" />
+              <img src={star} alt="" />
+              <h3>{currentTestimonial.name}</h3>
+              <p>{currentTestimonial.content}</p>
+            </div>
+            <div className="testi-box">
+              <img src={currentTestimonial.imgSrc} alt="" />
+              <img src={star} alt="" />
+              <h3>{currentTestimonial.name}</h3>
+              <p>{currentTestimonial.content}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
-export default Testimonial;
+  export default Testimonial;
