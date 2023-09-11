1ed9033a8f14ebf72e47d66d502f046880f84cef
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-09-11 11:45:10 +0530

    testimonial update

--- advati-homes/src/Components/Testimonial.js
+++ advati-homes/src/Components/Testimonial.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "../Style/Testimonial.css";
 import t1 from "../Assets/img/t1.svg";
 import t2 from "../Assets/img/t2.svg";
 import t3 from "../Assets/img/t3.svg";
 import star from "../Assets/img/star2.svg";
-import testi from "../Assets/img/testi.svg";
 import tright from "../Assets/img/tright.svg";
 import tleft from "../Assets/img/tleft.svg";
 
-const testimonials = [
+const Testimonial = () => {
+  const [testimonials, setTestimonials] = useState([
     {
       imgSrc: t1,
       name: "Hanna Franci",
@@ -16,63 +16,70 @@ const testimonials = [
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis nulla interdum orci congue rlibero. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     },
     {
-      imgSrc: "../Assets/img/t2.svg",
+      imgSrc: t2,
       name: "John Doe",
       content:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis nulla interdum orci congue rlibero. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     },
     {
-      imgSrc: "../Assets/img/t3.svg",
+      imgSrc: t3,
       name: "Jane Smith",
       content:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis nulla interdum orci congue rlibero. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     },
-  ];
+    {
+      imgSrc: t2,
+      name: "John Doe",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis nulla interdum orci congue rlibero. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      imgSrc: t1,
+      name: "Hanna Franci",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis nulla interdum orci congue rlibero. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    // Add more testimonials here
+  ]);
 
-  const Testimonial = () => {
-    const [currentIndex, setCurrentIndex] = useState(0);
+  const [startIndex, setStartIndex] = useState(0);
 
-    const handleNext = () => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % testimonials.length);
-    };
-
-    const handlePrev = () => {
-      setCurrentIndex((prevIndex) =>
-        prevIndex === 0 ? testimonials.length - 1 : prevIndex - 1
-      );
-    };
-
-    const currentTestimonial = testimonials[currentIndex];
-
-    return (
-      <div>
-        <div className="testi-index">
-          <div className="testimonial">
-            <img src={tright} alt="" id="tright" onClick={handleNext} />
-            <img src={tleft} alt="" id="tleft" onClick={handlePrev} />
-
-            <div className="testi-box">
-              <img src={currentTestimonial.imgSrc} alt="" />
-              <img src={star} alt="" />
-              <h3>{currentTestimonial.name}</h3>
-              <p>{currentTestimonial.content}</p>
-            </div>
-            <div className="testi-box testi-center">
-              <img src={currentTestimonial.imgSrc} alt="" />
-              <img src={star} alt="" />
-              <h3>{currentTestimonial.name}</h3>
-              <p>{currentTestimonial.content}</p>
-            </div>
-            <div className="testi-box">
-              <img src={currentTestimonial.imgSrc} alt="" />
-              <img src={star} alt="" />
-              <h3>{currentTestimonial.name}</h3>
-              <p>{currentTestimonial.content}</p>
-            </div>
-          </div>
-        </div>
-      </div>
+  const handleNext = () => {
+    setStartIndex((prevIndex) =>
+      prevIndex + 3 < testimonials.length ? prevIndex + 1 : 0
     );
   };
 
-  export default Testimonial;
+  const handlePrev = () => {
+    setStartIndex((prevIndex) =>
+      prevIndex > 0 ? prevIndex - 1 : testimonials.length - 3
+    );
+  };
+
+  const displayedTestimonials = testimonials.slice(
+    startIndex,
+    startIndex + 3
+  );
+
+  return (
+    <div>
+      <div className="testi-index">
+        <div className="testimonial">
+          <img src={tright} alt="" id="tright" onClick={handleNext} />
+          <img src={tleft} alt="" id="tleft" onClick={handlePrev} />
+
+          {displayedTestimonials.map((testimonial, index) => (
+            <div key={index} className="testi-box">
+              <img src={testimonial.imgSrc} alt="" />
+              <img src={star} alt="" />
+              <h3>{testimonial.name}</h3>
+              <p>{testimonial.content}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonial;
